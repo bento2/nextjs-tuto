942d7758cde748197303f94b88c52e30bc422132
Laurent Bodelet <laurent.bodelet@alchimie.com>
2023-12-01 10:22:08 +0100

    Correction form auth

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -1,4 +1,4 @@
-import type {NextAuthConfig} from 'next-auth';
+import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
     pages: {
@@ -9,7 +9,8 @@ export const authConfig = {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
-                return isLoggedIn;
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
             } else if (isLoggedIn) {
                 return Response.redirect(new URL('/dashboard', nextUrl));
             }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -11,8 +11,9 @@ import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
 
+
 export default function LoginForm() {
-  const [state, dispatch] = useFormState(authenticate, undefined);
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
   return (
     <form className="space-y-3" action={dispatch}>
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -61,17 +62,17 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        
+
         <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
+            className="flex h-8 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
         >
-          {state === 'CredentialsSignin' && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">Invalid credentials</p>
-            </>
+          {errorMessage && (
+              <>
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </>
           )}
         </div>
       </div>
